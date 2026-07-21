2c3c379c2afab0e3d5e17907ac99623ded17dae3
Wally <wesworldcrew@gmail.com>
2026-07-21 10:06:08 +0300

    Move favicon to public folder and link manually

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
